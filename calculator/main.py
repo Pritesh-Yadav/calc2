64d6f53690e0f1217735759f4e162b5037d6d464
Pritesh Ponnaboina <pritesh.ponnaboina@gmail.com>
2021-11-10 00:02:47 -0500

    update

--- calculator/main.py
+++ calculator/main.py
@@ -1,21 +1,28 @@
 """ This is the increment function"""
+
+
 class Calculator:
     """ This is the Calculator class"""
 
-    result = 0
-    def get_result(self):
-        """ Get Result of Calculation"""
-        return self.result
+    @staticmethod
+    def add_number(value_a, value_b):
+        """ adds two numbers"""
+        return value_a + value_b
 
-    def add_number(self, value_a):
-        """ adds number to result"""
-        self.result = self.result + value_a
-        return self.result
-    def subtract_number(self, value_a):
-        """ subtract number from result"""
-        self.result = self.result - value_a
-        return self.result
-    def multiply_numbers(self, value_a, value_b):
-        """ multiply two numbers and store the result"""
-        self.result = value_a * value_b
-        return self.result
+    @staticmethod
+    def subtract_number(value_a, value_b):
+        """ subtract two numbers """
+        return value_a - value_b
+
+    @staticmethod
+    def multiply_numbers(value_a, value_b):
+        """ multiply two numbers """
+        return value_a * value_b
+
+    @staticmethod
+    def divide_numbers(value_a, value_b):
+        """ divide two numbers """
+        try:
+            return value_a / value_b
+        except ZeroDivisionError as myer:
+            raise ZeroDivisionError from myer
